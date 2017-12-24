40175292d69783f9b481d6fbfedecc6692b16949
Jorgen Phillips <jorgenphillips@gmail.com>
2017-12-24 12:01:46 -0600

    MB4 for full unicode support

--- app/includes/settings.default.php
+++ app/includes/settings.default.php
@@ -9,6 +9,6 @@
 define('JEE_PRETTYURLS', false); // Use /read.php/boardname/1400999437/ instead of /read.php?b=boardname&t=1400999437 (requires URL rewriting)
 define('JEE_SALT', "changeme"); // Enter some random data, and don't change this in the future (used for secure tripcodes)
 
-define('JEE_PDODSN', "mysql:host=localhost;dbname=changeme;charset=utf8");
+define('JEE_PDODSN', "mysql:host=localhost;dbname=changeme;charset=utf8mb4");
 define('JEE_PDOUSER', "changeme");
 define('JEE_PDOPASS', "changeme");
